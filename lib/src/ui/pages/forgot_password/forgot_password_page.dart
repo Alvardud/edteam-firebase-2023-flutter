7367c1c3943cfd114afecd660c734136b9c2b9ce
Alvardud <alvardud@gmail.com>
2023-10-23 22:41:43 -0400

    fix: agregado de key en forgot password

--- lib/src/ui/pages/forgot_password/forgot_password_page.dart
+++ lib/src/ui/pages/forgot_password/forgot_password_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+GlobalKey<ScaffoldMessengerState> rememberKey =
+    GlobalKey<ScaffoldMessengerState>();
+
 class ForgotPasswordPage extends StatefulWidget {
   static const route = 'forgot_password_page';
   const ForgotPasswordPage({super.key});
@@ -23,35 +26,38 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Olvidé mi Contraseña'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Por favor, ingrese su dirección de correo electrónico para restablecer su contraseña.',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              TextField(
-                controller: correoController,
-                decoration: const InputDecoration(
-                  labelText: 'Correo Electrónico',
+    return ScaffoldMessenger(
+      key: rememberKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Olvidé mi Contraseña'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Por favor, ingrese su dirección de correo electrónico para restablecer su contraseña.',
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Aquí puedes agregar la lógica para enviar un correo de restablecimiento de contraseña
-                },
-                child: const Text('Enviar Correo de Restablecimiento'),
-              ),
-            ],
+                const SizedBox(height: 20),
+                TextField(
+                  controller: correoController,
+                  decoration: const InputDecoration(
+                    labelText: 'Correo Electrónico',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Aquí puedes agregar la lógica para enviar un correo de restablecimiento de contraseña
+                  },
+                  child: const Text('Enviar Correo de Restablecimiento'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
